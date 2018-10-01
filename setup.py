68ac14f15620167bb4f1548b3d29ca5b64ae39c9
rtix <radif.tazetdinov@mail.ru>
2018-10-01 20:41:53 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyyandextranslateapi",
+    version="1.0",
+    author="Radif rtix Tazetdinov",
+    author_email="radif.tazetdinov@mail.ru",
+    description="Implementation of Yandex.Translate API as a Python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rtix/pyYandexTranslateAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
